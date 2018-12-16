64c009f363ebcdfca60d560357ba57978b8d2230
ervinjusufagic <ervin_j_@hotmail.com>
2018-12-16 12:09:44 +0100

    created build

---
+++ build/precache-manifest.407deb5ff158148e25323a01346af08a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72dff8ae5aa67848c680",
+    "url": "/static/js/main.72dff8ae.chunk.js"
+  },
+  {
+    "revision": "b1bc0e55a72bb937fda9",
+    "url": "/static/js/1.b1bc0e55.chunk.js"
+  },
+  {
+    "revision": "72dff8ae5aa67848c680",
+    "url": "/static/css/main.bf83fab5.chunk.css"
+  },
+  {
+    "revision": "16353999758ce60b97ac0bf0298deea5",
+    "url": "/index.html"
+  }
+];
